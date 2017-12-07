1ba5ba85fe25739563665bc46a203aae4d99b514
clodoaldoBasaglia <clodoaldofonseca92@gmail.com>
2017-12-07 01:42:05 -0200

    llvmlite e testes adicionados

---
+++ geracao-codigo-testes/gencode-003.tpp
@@ -0,0 +1,17 @@
+{Condicional}
+inteiro: a
+
+inteiro principal()
+	inteiro: ret
+	a := 10    
+	se a > 5 então
+        se a < 20 então
+        	ret := 1
+        senão
+        	ret := 2
+    senão
+        ret := 0
+    fim
+
+    retorna(0)
+fim
--- new/geracao.py
+++ new/geracao.py
@@ -1,7 +1,30 @@
 from llvmlite import ir
+from sintatica import *
+from semantica import *
 
-modulo = ir.Module("modulo.bc")
-f = open('myMode.ll','w')
-f.write(str(modulo))
-f.close()
-print(modulo)
+class Generator():
+    def __init__(self, code, optz= True, debug=True):
+        s = Semantica(code.read())
+        s.lista_declaracoes(self)
+        self.tree=s.tree
+        self.table = s.simbolos
+        self.builder=None
+        self.modulo = ir.Module("programaModulo")
+        self.scope = "global"
+        self.func = None
+        self.printf= ir.Function(self.modulo, ir.FunctionType(ir.FloatType(), [ir.FloatType()]), "printf_f")
+        self.scanf= ir.Function(self.modulo, ir.FunctionType(ir.FloatType(), [ir.FloatType()]), "scanf_f")
+        self.inicioGeneracao(self.tree)
+        print(self.modulo)
+    def inicioGeneracao(self,node):
+        print("oi")
+        if self.tree.type == "programa":
+            self.programa(self.tree.child[0])
+    def lista_declaracoes(self, node):
+        print("oi")
+
+
+if __name__=="__main__":
+    import sys,io
+    code = io.open(sys.argv[1], mode="r", encoding="utf-8")
+    driver = Generator(code)
